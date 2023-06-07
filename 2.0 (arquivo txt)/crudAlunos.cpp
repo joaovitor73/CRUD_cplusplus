e4e824c89ff7037ceb4b30a6664a04c61c1cb562
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-06-07 08:15:12 -0300

    Update crudAlunos.cpp

--- 2.0 (arquivo txt)/crudAlunos.cpp
+++ 2.0 (arquivo txt)/crudAlunos.cpp
@@ -10,34 +10,32 @@ struct tAlunos{
 };
 
 const short int tam = 3;
+
 int main(){
+
     fstream arquivo;
-    string nom, linha;
-    string aux;
-    short int cont = 0;
-    float notas[3];
-    short int op, i = 0, op2, op3;
-    short int matricula;
+    string aux, linha, nome, linhaAux;
+    short int op, op2, op3, fim = 0, matricula, cont, cont2;
     bool removido = false, flag, nAchei;
-    float nota;
+    float notas[3],nota;
     tAlunos alunos[tam];
-    do{ cout << "\n------------------------------"; 
-        cout << "\n\tMENU PRIMARIO";
-        cout << "\n------------------------------";
-        cout << "\n1-Cadastrar alunos";
+
+    do{ cout << "\n--------------------------------"; 
+        cout << "\n\t  MENU PRIMARIO";
+        cout << "\n--------------------------------"; 
+        cout << "\n\n1-Cadastrar alunos";
         cout << "\n2-Remover alunos";
-        cout << "\n3-Relatorio";
+        cout << "\n3-Listar alunos";
         cout << "\n4-Alterar dados";
         cout << "\n5-Sair\nopcao:";
         cin >> op;
+        cout << "\e[H\e[2J";
         switch(op){
-            case 1: if(i < tam){
-                        cout << "\n------------------------------";
-                        cout << "\n\tCADASTRO";
-                        cout << "\n------------------------------";
-                    //salvando no txt
-                    arquivo.open("date.txt");
-                    do{ 
+            case 1: cout << "\n--------------------------------"; 
+                    if(fim < tam){
+                        cout << "\n\t  CADASTRO";
+                        cout << "\n--------------------------------"; 
+                        arquivo.open("date.txt");                   
                         do{ cout << "\nDigite a matricula do aluno: ";
                             cin >> matricula;
                         }while(matricula < 0);//impedir matriculas negativas
@@ -45,49 +43,50 @@ int main(){
                         cont = 0;
                         aux = to_string(matricula);
                         while(getline(arquivo,linha)){
-                            if(cont == 0){
-                                cout << linha << "\n" << aux << "\n\n";
-                                if (linha == aux) flag = true;
+                            cont2 = 0;
+                            while(linha[cont2] != ','){
+                                linhaAux += linha[cont2];
+                                cont2++;
                             }
-                            cont++;
-                            if(cont == 5) cont = 0;
-
+                            if (linhaAux == aux) flag = true;
+                            linhaAux = "";
                         }
+                        arquivo.close();
                         if(flag) cout << "\nMatricula ja existente, digite novamente!\n";
-                    }while(flag);//impedir matriculas iguais
-                    arquivo.close();
-                    cout << "\nDigite o nome do aluno: ";
-                    getchar();
-                    getline(cin, nom);
-                    for(int j = 0; j < 3; j++){
-                        do{ cout << "\nDigite a "  << j+1 << "a. nota: ";
-                            cin >> notas[j];
-                        }while(notas[j] > 10 || notas[j] < 0);
-                    }
-                    cout << "------------------------------";
-                    i++;
-                    arquivo.open("date.txt",  ios::app);
-                    arquivo << matricula;
-                    arquivo << "\n" << nom << "\n";
-                    arquivo << notas[0] << "\n" << notas[1] << "\n" << notas[2] << "\n";
-                    arquivo.close();
-                    }else{
-                        cout << "\nCadastro cheio!";
-                    }
+                        else{
+                            cout << "\nDigite o nome do aluno: ";
+                            getchar();
+                            getline(cin, nome);
+                            for(int j = 0; j < 3; j++){
+                                do{ cout << "\nDigite a "  << j+1 << "a. nota: ";
+                                    cin >> notas[j];
+                                }while(notas[j] > 10 || notas[j] < 0);
+                            }
+                            cout << "------------------------------";
+                            arquivo.open("date.txt",  ios::app);
+                            arquivo << matricula;
+                            arquivo << ", " << nome << ", ";
+                            arquivo << notas[0] << ", " << notas[1] << ", " << notas[2] << ",\n";
+                            arquivo.close();  
+                            fim++;                       
+                        }
+                    }else cout << "\n\nLista de alunos cheia!";                  
+                    cout << "\n--------------------------------";
                 break;
-            case 2: if(i > 0){
+            case 2: cout << "\n--------------------------------"; 
+                    if(fim > 0){ 
                         removido = false;
-                        cout << "\n------------------------------";
+                        cout << "\n--------------------------------"; 
                         cout << "\n\nDigite a matricula do aluno: ";
                         cin >> matricula;
-                        for(int j = 0; j < i; j++){
+                        for(int j = 0; j < fim; j++){
                             flag = false;
                             if(matricula == alunos[j].matricula){
                                 alunos[j].matricula = -1;
                                 removido = true;
                                 flag = true;
                             }
-                            if((removido) && (j < i-1)){
+                            if((removido) && (j < fim-1)){
                                 alunos[j].matricula = alunos[j+1].matricula;
                                 alunos[j].nome = alunos[j+1].nome;
                                 if(flag) alunos[j+1].matricula = -1;
@@ -95,87 +94,99 @@ int main(){
 
                         }
                         if(removido){
-                            i--;
-                            cout << "\nRemocao realizada com sucesso!!\n";
+                            fim--;
+                            cout << "\nAluno removido com sucesso!!\n";
                         } 
-                        else  cout << "\nMatricula nao  encontrada!\n";
-                        cout << "\n------------------------------";
-                }else cout << "\nNenhum aluno cadastrado no momento!\n";
+                        else  cout << "\n\nA matricula digitada nao foi encontrada!\n";                 
+                    }else cout << "\n\nLista de alunos vazia!\n";
+                    cout << "\n--------------------------------"; 
                 break;
             case 3: flag = true;
+                    cout << "\n--------------------------------"; 
                     arquivo.open("date.txt",  ios::in);
+                    cont = 0;
                     if(arquivo.is_open()){
-                        cout << "\n------------------------------";
-                        cout << "\n\tRELATORIO";
-                        cout << "\n------------------------------";
+                        cout << "\n\t  RELATORIO";
+                        cout << "\n--------------------------------"; 
                         while(getline(arquivo,linha)){
-                            flag = false;
-                            cont++;
-                            switch(cont){
-                                case 1: cout << "\nMatricula do aluno: " << linha;
-                                    break;
-                                case 2: cout << "\n\nNome do aluno: " << linha;
-                                    break;
-                                case 3:cout << "\n\nNotas: " << linha;
-                                    break;
-                                case 4: cout << " " << linha;
-                                    break;
-                                case 5: cout << " " <<linha;
-                                    break;
+                            flag = false;            
+                            for (char ch : linha) {
+                                if (ch != ',')  linhaAux += ch;
+                                else{
+                                    cont++;                         
+                                    switch(cont){
+                                        case 1: cout << "\nMatricula do aluno: " << linhaAux;
+                                            break;
+                                        case 2: cout << "\nNome do aluno: " << linhaAux;
+                                            break;
+                                        case 3:cout << "\nNotas: " << linhaAux;
+                                            break;
+                                        case 4: cout << " " << linhaAux;
+                                            break;
+                                        case 5: cout << " " <<linhaAux;
+                                            break;
+                                    }
+                                    linhaAux = ""; 
+                                }
+                                if(cont == 5){
+                                    cout << "\n------------------------------";
+                                    cont = 0;
+                                } 
                             }
-                            if(cont == 5){
-                                cout << "\n------------------------------";
-                                cont = 0;
-                            } 
+                            
                         }
                         arquivo.close();
                         if(flag) cout << "\nBanco de dados vazio!";
                     }
                 break;
-            case 4: if(i > 0){
-                        cout << "\n------------------------------";
+            case 4: cout << "\n--------------------------------"; 
+                    if(fim > 0){ 
+                        cout << "\n--------------------------------"; 
                         cout << "\n\nDigite a matricula do aluno: ";
                         cin >> matricula;
                         nAchei = true;
-                        for(int j = 0; j < i; j++){
+                        for(int j = 0; j < fim; j++){
                             if(matricula == alunos[j].matricula){
                                 nAchei = false;
                                 do{
-                                    cout << "\n------------------------------"; 
-                                    cout << "\n\tMENU SECUNDARIO";
-                                    cout << "\n------------------------------";
-                                    cout << "\n1-Alterar matricula";
+                                    cout << "\n--------------------------------"; 
+                                    cout << "\n\t  MENU SECUNDARIO";
+                                    cout << "\n--------------------------------"; 
+                                    cout << "\n\n1-Alterar matricula";
                                     cout << "\n2-Alterar nome";
                                     cout << "\n3-Alterar notas";
                                     cout << "\n4-Sair\nopcao:";
                                     cin >> op2;
                                     cout << "\n\n--------------------------------";
+                                    cout << "\e[H\e[2J";
                                     switch(op2){
-                                        case 1: do{
-                                                    do{ cout << "\nDigite  nova a matricula do aluno: ";
-                                                        cin >> matricula;
-                                                    }while(matricula < 0); //impedir matriculas negativas
-                                                    flag =  false;
-                                                    for(int j = 0; j < i; j++){
-                                                        if(alunos[j].matricula == matricula) flag = true;
-                                                    }
-                                                    if(flag) cout << "\nMatricula ja existente, digite novamente!\n";
-                                                    else alunos[i].matricula = matricula;
-                                                }while(flag);//impedir matriculas iguais
+                                        case 1: do{ cout << "\nDigite  nova a matricula do aluno: ";
+                                                    cin >> matricula;
+                                                }while(matricula < 0); //impedir matriculas negativas
+                                                flag =  false;
+                                                for(int j = 0; j < fim; j++){
+                                                    if(alunos[j].matricula == matricula) flag = true;
+                                                }
+                                                if(flag){ 
+                                                    cout << "\nEssa matricula ja existe em nosso sistema!\n";
+                                                    cout << "\n--------------------------------"; 
+                                                }
+                                                else alunos[j].matricula = matricula;
                                             break;
                                         case 2: getchar();
                                                 cout << "\nDigite o novo nome do aluno: ";
                                                 getline(cin, alunos[j].nome);
+                                                cout << "\n--------------------------------"; 
                                             break;
-                                        case 3: cout << "\n------------------------------";
-                                                cout << "\n\tMENU TERCIARIO";
-                                                cout << "\n------------------------------";
-                                                cout << "\n1-Alterar a primeira nota";
+                                        case 3: cout << "\n--------------------------------"; 
+                                                cout << "\n\t  MENU TERCIARIO";
+                                                cout << "\n--------------------------------"; 
+                                                cout << "\n\n1-Alterar a primeira nota";
                                                 cout << "\n2-Alterar a segunda nota";
                                                 cout << "\n3-Alterar a terceira nota";
                                                 cout << "\n4-Sair\nopcao:";
                                                 cin >> op3;
-                                                cout << "\n--------------------------------";
+                                                cout << "\n--------------------------------"; 
                                                 do{cout << "\nDigite a nota: ";
                                                     cin >> nota;
                                                 }while(nota < 0 || nota > 10);
@@ -190,6 +201,7 @@ int main(){
                                                         break;
                                                     default: cout << "\nValor invalido, digite novamente!";                                                     
                                                 }
+                                                cout << "\n--------------------------------"; 
                                             break;
                                         case 4: cout << "\n\nSaindo...";
                                             break;
@@ -198,9 +210,10 @@ int main(){
                                 }while(op2 != 4);
                             }
                         }
-                        if(nAchei) cout << "\n\nMatricula nao encontrada!\n";
-                        else cout << "\n\nAtualizacao realizada com sucesso!\n";
-                    }else cout << "\nNenhum aluno cadastrado no momento!\n";
+                        if(nAchei) cout << "\nA matricula digitada nao foi encontrada!\n";
+                        else cout << "\n\nA atualizacao dos dados foi realizada com sucesso!\n";
+                    }else cout << "\n\nLista de alunos vazia!\n";
+                    cout << "\n--------------------------------"; 
                 break;
             case 5: cout << "\nSaindo..."; 
                 break;
